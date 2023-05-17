9613a67879cc581369f36161dfda20993612b750
csviri <csviri@gmail.com>
2023-05-17 10:06:58 +0200

    remove tests

--- src/test/java/io/csviri/RedisSyncControllerIT.java
+++
@@ -1,8 +0,0 @@
-package io.csviri;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class RedisSyncControllerIT extends RedisSyncControllerTest {
-    // Execute the same tests but in packaged mode.
-}
